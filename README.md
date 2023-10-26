fcbfcbab818aad7cd71c1a8ad5f3f388cce0a373
steewel <steewel@gmail.com>
2023-10-26 18:49:19 -0300

    Incluídos os arquivos do projeto

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# desenvolvimento_mobile
+# HamburgueriaZ
+
+Esse projeto foi desenvolvido com o propósito de criar um aplicativo para uma hamburgueria, utilizando Java no ambiente Android Studio, permitindo que os clientes façam seus pedidos diretamente pelo app. Para configurar o ambiente, siga os passos abaixo:
+
+1. Abra o Android Studio.
+2. Conecte um celular Android ao computador.
+3. No Android Studio, clique em "Executar app".
---
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.hamburgueriaz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CheckBox baconCheckBox;
+    private TextView amountValueTextView;
+    private CheckBox onionRingCheckBox;
+    private Button orderButton, addValue, reduceValue;
+    private CheckBox cheeseCheckBox;
+    private TextView totalOrderValueTextView;
+
+    private double totalOrderValue = 0.00;
+
+    private int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // start views
+        amountValueTextView = findViewById(R.id.amountValue);
+        baconCheckBox = findViewById(R.id.baconCheckBox);
+        cheeseCheckBox = findViewById(R.id.cheeseCheckbox);
+        onionRingCheckBox = findViewById(R.id.onionRingCheckBox);
+        totalOrderValueTextView = findViewById(R.id.totalOrderValue);
+        orderButton = findViewById(R.id.orderButton);
+        addValue = findViewById(R.id.addValue);
+        reduceValue = findViewById(R.id.reduceValue);
+
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        orderButton.setOnClickListener(view -> sendOrder());
+        addValue.setOnClickListener(view -> addQuantity());
+        reduceValue.setOnClickListener(view -> removeQuantity());
+    }
+
+    // Função para subtrair a quantidade
+    private void removeQuantity() {
+        if (quantity > 0) {
+            quantity--;
+            updateQuantity(quantity);
+        }
+    }
+
+    // Função para adicionar a quantidade
+    private void addQuantity() {
+        if (quantity >= 0) {
+            quantity++;
+            updateQuantity(quantity);
+        }
+    }
+
+    // Função para atualizar a quantidade na view
+    private void updateQuantity(int quantity) {
+        amountValueTextView.setText(String.valueOf(quantity));
+    }
+
+    // Função para calcular o valor total do pedido
+    private void calculeTotalOrder() {
+        totalOrderValue = (quantity * 20.00) +
+                (baconCheckBox.isChecked() ? 2.00 : 0.00) +
+                (cheeseCheckBox.isChecked() ? 2.00 : 0.00) +
+                (onionRingCheckBox.isChecked() ? 3.00 : 0.00);
+
+    }
+
+    // Função para enviar o pedido
+    private void sendOrder() {
+        calculeTotalOrder();
+
+        TextInputEditText userName = findViewById(R.id.inputText);
+        StringBuilder resumeOrder = new StringBuilder();
+        resumeOrder.append("Nome: ").append(userName.getText()).append("\n");
+        resumeOrder.append("Quantidade: ").append(quantity).append("\n");
+
+        if (baconCheckBox.isChecked()) {
+            resumeOrder.append("Adicional: Bacon\n");
+        }
+        if (cheeseCheckBox.isChecked()) {
+            resumeOrder.append("Adicional: Queijo\n");
+        }
+        if (onionRingCheckBox.isChecked()) {
+            resumeOrder.append("Adicional: Onion Rings\n");
+        }
+
+        resumeOrder.append("Total: R$ ").append(totalOrderValue);
+
+        totalOrderValueTextView.setText(resumeOrder.toString());
+
+        composeEmail("hamburgueriaZ@email.com.br", "Pedido de " + userName.getText(), resumeOrder.toString());
+    }
+
+    private void composeEmail(String email, String subject, String message) {
+
+        final Intent result = new Intent(android.content.Intent.ACTION_SEND);
+        result.setType("plain/text");
+        result.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] { email });
+        result.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+        result.putExtra(android.content.Intent.EXTRA_TEXT, message);
+        startActivity(Intent.createChooser(result, "Send mail..."));
+    }
+}
